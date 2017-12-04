46afb83c87a2c1aed0356dd6e7c2f437a378786d
Surya Widi <neversleepman@tfwno.gf>
2017-12-04 23:44:49 +0700

    Initial commit

---
+++ generate.js
@@ -0,0 +1,66 @@
+const fs = require('fs')
+const Mustache = require('mustache')
+const orgs = require('./out/data.json')
+
+const template = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>GCI Leaders</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <style>
+      body {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica,
+          Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+          "Segoe UI Symbol";
+        line-height: 1.5;
+        max-width: 800px;
+        margin: 0 auto;
+        padding: 0 1em;
+      }
+
+      h1 {
+        margin: .5em 0 0 0;
+      }
+
+      .orgs {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+      }
+
+      .org {
+        flex: 0 0 25%;
+        margin-bottom: 1em;
+      }
+
+      ul {
+        padding-left: 1em;
+        margin: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>GCI Current Leaders</h1>
+    <i>Names are listed alphabetically</i>
+    <div class="orgs">
+      {{#orgs}}
+        <div class="org">
+          <h3>{{name}}</h3>
+          <ul>
+            {{#leaders}}
+              <li>{{display_name}}</li>
+            {{/leaders}}
+            {{^leaders}}
+              <li style="color: gray;">None</li>
+            {{/leaders}}
+          </ul>
+        </div>
+      {{/orgs}}
+    </div>
+  </body>
+</html>
+`
+
+fs.writeFileSync('out/index.html', Mustache.render(template, { orgs }))
---
+++ scrap.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+
+async function fetchOrgs() {
+  const res = await fetch(
+    'https://codein.withgoogle.com/api/program/2017/organization/?status=2'
+  )
+  const { results } = await res.json()
+  return results
+}
+
+async function fetchLeaders(id) {
+  const res = await fetch(
+    `https://codein.withgoogle.com/api/program/current/organization/${id}`
+  )
+  const { leaders } = await res.json()
+  return leaders
+}
+
+async function fetchOrgsWithLeaders() {
+  const orgs = await fetchOrgs()
+  const fetchingLeaders = orgs.map(org => fetchLeaders(org.id))
+  const orgLeaders = await Promise.all(fetchingLeaders)
+
+  return orgs.map((org, index) =>
+    Object.assign(org, {
+      leaders: orgLeaders[index],
+    })
+  )
+}
+
+;(async () => {
+  const data = await fetchOrgsWithLeaders()
+  fs.writeFileSync('out/data.json', JSON.stringify(data))
+})()
