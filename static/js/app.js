d4a895042c7e6f90c02dd04842eed05e63f43831
groxni <nishukai@hotmail.com>
2017-12-10 20:19:51 +0800

    Add timeline and countdown
    This commit adds timeline with countdown.
    
    Closes https://github.com/coala/gci-leaders/issues/51

--- static/js/app.js
+++ static/js/app.js
@@ -35,3 +35,26 @@ allLeaders.forEach(function(orgList) {
     orgList.appendChild(orgList.children[(Math.random() * i) | 0])
   }
 })
+
+document.getElementById('progress').textContent = gciProgress()
+
+function gciProgress() {
+  const progress = document.getElementById('progress').dataset
+  const competitionOpen = new Date(progress.competitionopen)
+  const noClaims = new Date(progress.noclaims)
+  const current = new Date()
+  const percentagePassed =
+    100 - (noClaims - current) / (noClaims - competitionOpen) * 100
+  document
+    .getElementsByClassName('progress-bar')[0]
+    .setAttribute(
+      'style',
+      'width:' + percentagePassed + '%;height: 8px;border-radius: 100px;'
+    )
+  return (
+    Math.round(percentagePassed) +
+    '% passed, ' +
+    timeDifference(noClaims, current).slice(0, -4) +
+    ' before task claiming ends'
+  )
+}
