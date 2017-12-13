2899de985a6aee5fb9c8c2d1c9f86fb796c5d678
Marcos Gómez <margobra8@gmail.com>
2017-12-13 20:28:48 +0100

    i18n: Add full i18n support
    This adds i18n support for the app, being
    rendered in client-side so that translations
    can be loaded dynamically. Implemented language
    fallbacks for non-existing translations, plurals
    and dynamic placeholders for strings.
    
    Fixes https://github.com/coala/gci-leaders/issues/38

--- static/js/app.js
+++ static/js/app.js
@@ -13,17 +13,44 @@ function timeDifference(current, previous) {
   const elapsed = current - previous
 
   if (elapsed < msPerMinute) {
-    return Math.round(elapsed / 1000) + ' seconds ago'
+    return $.i18n('seconds-ago', Math.round(elapsed / 1000))
   } else if (elapsed < msPerHour) {
-    return Math.round(elapsed / msPerMinute) + ' minutes ago'
+    return $.i18n('minutes-ago', Math.round(elapsed / msPerMinute))
   } else if (elapsed < msPerDay) {
-    return Math.round(elapsed / msPerHour) + ' hours ago'
+    return $.i18n('hours-ago', Math.round(elapsed / msPerHour))
   } else if (elapsed < msPerMonth) {
-    return 'approximately ' + Math.round(elapsed / msPerDay) + ' days ago'
+    return $.i18n('days-ago', Math.round(elapsed / msPerDay))
   } else if (elapsed < msPerYear) {
-    return 'approximately ' + Math.round(elapsed / msPerMonth) + ' months ago'
+    return $.i18n('months-ago', Math.round(elapsed / msPerMonth))
   } else {
-    return 'approximately ' + Math.round(elapsed / msPerYear) + ' years ago'
+    return $.i18n('years-ago', Math.round(elapsed / msPerYear))
+  }
+}
+
+// this is needed for time expression translation.
+// will be removed when we find a way
+// to correctly strip the "ago" segment of a translation
+function timeDifferenceFuture(current, previous) {
+  const msPerMinute = 60 * 1000
+  const msPerHour = msPerMinute * 60
+  const msPerDay = msPerHour * 24
+  const msPerMonth = msPerDay * 30
+  const msPerYear = msPerDay * 365
+
+  const elapsed = current - previous
+
+  if (elapsed < msPerMinute) {
+    return $.i18n('seconds', Math.round(elapsed / 1000))
+  } else if (elapsed < msPerHour) {
+    return $.i18n('minutes', Math.round(elapsed / msPerMinute))
+  } else if (elapsed < msPerDay) {
+    return $.i18n('hours', Math.round(elapsed / msPerHour))
+  } else if (elapsed < msPerMonth) {
+    return $.i18n('days', Math.round(elapsed / msPerDay))
+  } else if (elapsed < msPerYear) {
+    return $.i18n('months', Math.round(elapsed / msPerMonth))
+  } else {
+    return $.i18n('years', Math.round(elapsed / msPerYear))
   }
 }
 
@@ -51,10 +78,9 @@ function gciProgress() {
       'style',
       'width:' + percentagePassed + '%;height: 8px;border-radius: 100px;'
     )
-  return (
-    Math.round(percentagePassed) +
-    '% passed, ' +
-    timeDifference(noClaims, current).slice(0, -4) +
-    ' before task claiming ends'
+  return $.i18n(
+    'passed',
+    Math.round(percentagePassed),
+    timeDifferenceFuture(noClaims, current)
   )
 }
