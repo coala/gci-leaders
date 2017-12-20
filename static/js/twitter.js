73b5ea2f955809824b7fb3a1e68a6dc92a6bf71e
dob9601 <dob9601@gmail.com>
2017-12-20 23:39:08 +0000

    Addition and implementation of twitter filter
    closes https://github.com/coala/gci-leaders/issues/54

--- static/js/twitter.js
+++ static/js/twitter.js
@@ -8,5 +8,6 @@
   js = d.createElement(s)
   js.id = id
   js.src = p + '://platform.twitter.com/widgets.js'
+  js.setAttribute('load', 'twitter_filter()')
   fjs.parentNode.insertBefore(js, fjs)
 })(document, 'script', 'twitter-wjs')
